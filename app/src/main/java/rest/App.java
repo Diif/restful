cbd49d9cced82b8576a3e243c323c2d8ee008732
prozella <kiwulik@yandex.ru>
2022-07-07 23:11:13 +0700

    refactoring

--- app/src/main/java/rest/App.java
+++ app/src/main/java/rest/App.java
@@ -14,38 +14,17 @@ import rest.model.ShopUnitImport;
 import rest.model.ShopUnitType;
 import rest.repositories.ShopUnitImportRepository;
 
+
 import javax.validation.Valid;
 import java.net.http.HttpResponse;
 import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
-@RestController
 public class App {
-    private ShopUnitImportRepository repo;
-
-    public ShopUnitImportRepository getRepo() {
-        return repo;
-    }
-
-    @Autowired
-    public void setRepo(ShopUnitImportRepository repo) {
-        this.repo = repo;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-//    @PostMapping("/imports")
-//    public ResponseEntity<ShopUnitImport> sayHello(@Valid @RequestBody ShopUnitImport shopUnitImport){
-//        Optional<ShopUnitImport> optional =  repo.findById(shopUnitImport.getId());
-//        if(optional.isPresent()){
-//            if(optional.get().getType() != shopUnitImport.getType()){
-//                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//            }
-//        }
-//        repo.save(shopUnitImport);
-//        return new ResponseEntity<>(shopUnitImport,HttpStatus.OK);
-//    }
 }
