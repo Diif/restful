e882ee320648698c56ff984e38939a5a0026c475
prozella <kiwulik@yandex.ru>
2022-07-14 18:26:23 +0700

    repository refactor and fix

--- app/src/main/java/rest/repositories/ShopUnitRepositoryCustom.java
+++ app/src/main/java/rest/repositories/ShopUnitRepositoryCustom.java
@@ -4,11 +4,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.model.ShopUnit;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
+//TODO убрать хард-код, имени таблицы shop_unit не должно быть
 public interface ShopUnitImportRepositoryCustom {
     @Query(value = """
                 WITH RECURSIVE generation AS (
@@ -27,6 +27,28 @@ public interface ShopUnitImportRepositoryCustom {
                 )
                 \s
                 SELECT id,name,parent_id,price,type,update_date
-                FROM generation;""", nativeQuery = true)
+                FROM generation;"""
+            , nativeQuery = true)
     List<ShopUnit> getParentWithChildren(UUID id);
+
+    @Query(value = """
+                WITH RECURSIVE generation AS (
+                    SELECT *,
+                        0 AS generation_number
+                    FROM shop_unit
+                    WHERE id = ?
+                \s
+                UNION ALL
+                \s
+                    SELECT child.*,
+                        generation_number+1 AS generation_number
+                    FROM shop_unit child
+                    JOIN generation g
+                      ON g.id = child.parent_id
+                )
+                \s
+                SELECT id
+                FROM generation;"""
+            , nativeQuery = true)
+    List<UUID> getUUIDsParentWithChildren(UUID id);
 }
